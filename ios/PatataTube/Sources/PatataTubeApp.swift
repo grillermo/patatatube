5b93a24079a3b81d91f4e780465ac5cc8d244e41
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-07-23 16:25:41 -0600

    Install sentry

--- ios/PatataTube/Sources/PatataTubeApp.swift
+++ ios/PatataTube/Sources/PatataTubeApp.swift
@@ -1,5 +1,7 @@
 // ios/PatataTube/Sources/PatataTubeApp.swift
 import SwiftUI
+import Sentry
+
 import Capture
 
 @MainActor
@@ -22,6 +24,33 @@ struct PatataTubeApp: App {
     @Environment(\.scenePhase) private var scenePhase
 
     init() {
+        SentrySDK.start { options in
+            options.dsn = "https://de3e08718ecce35b4b92a8519ec40a79@o4511260455796736.ingest.us.sentry.io/4511786930798597"
+
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+
+            // Configure profiling. Visit https://docs.sentry.io/platforms/apple/profiling/ to learn more.
+            options.configureProfiling = {
+                $0.sessionSampleRate = 1.0 // We recommend adjusting this value in production.
+                $0.lifecycle = .trace
+            }
+
+            // Uncomment the following lines to add more data to your events
+            // options.attachScreenshot = true // This adds a screenshot to the error events
+            // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+            
+            // Enable experimental logging features
+            options.experimental.enableLogs = true
+        }
+        // Remove the next line after confirming that your Sentry integration is working.
+        SentrySDK.capture(message: "This app uses Sentry! :)")
+
         Logger.start(
             withAPIKey: "GiDZTDyGAIXregVT4+YK7n/iskOtnE+sDmoxpMpusDaGACILRVdwMVdOWUFDeFEouAw=",
             sessionStrategy: .fixed()
