505780b2630fec2fb5fbb2dcf9ccaa7a2a87190d
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2026-08-04 11:57:47 -0600

    build(ios): raise deployment target to iOS 18

--- ios/PatataTubeKit/Package.swift
+++ ios/PatataTubeKit/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "PatataTubeKit",
-    platforms: [.iOS(.v17), .macOS(.v14)],
+    platforms: [.iOS(.v18), .macOS(.v14)],
     products: [
         .library(name: "PatataTubeKit", targets: ["PatataTubeKit"]),
     ],
